2c2294ddfe29ca9d3d0d2d5501c56982425118f1
KashifCh_eth <103598065+KashifCh-eth@users.noreply.github.com>
2023-11-19 17:21:13 +0500

    Update accessmodifiers.sol

--- access modifiers in Solidity USDE Private,Internal,External,Public/accessmodifiers.sol
+++ access modifiers in Solidity USDE Private,Internal,External,Public/accessmodifiers.sol
@@ -1,1 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
 
+// There are four types of access modifiers available in Solidity.
+
+abstract contract parttwo {
+    uint256 public number;
+
+    function UpdateNumberwithpublic() public {
+        //   It is used to grant access within the contract as well as outside of the contract.
+        number++;
+    }
+
+    function UpdateNumberwithExternal() external {
+        // It is used to grant access only outside of the contract.
+        number++;
+    }
+
+    function UpdateNumberwithInternal() internal {
+        // It is used to grant access within the contract and all the contracts that inherit i
+        number++;
+    }
+
+    function UpdateNumberwithPrivate() private {
+        //Private: It is used to grant access only within a contract.
+        number++;
+    }
+}
+
+contract part is parttwo {
+    function publicUpdate() public {
+        // grant access within the contract and all the contracts that inherit
+        UpdateNumberwithInternal();
+    }
+}
